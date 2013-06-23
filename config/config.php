1a3e7bb42b2aadbd599b7404aa07bde870813545
Noah Pedrini <go@looklisten.net>
2013-06-23 16:18:14 -0400

    removed some private config values

--- config/config.php
+++ config/config.php
@@ -1,28 +1,33 @@
 <?php
 
-define("ADMIN_EMAIL","dpatman69@gmail.com");
+/*	recipient for feedback link in footer	*/
+define("ADMIN_EMAIL","");
 
+/*	set theme based on cookie (0=white,1=black)	*/
 define("THEME", isset($_COOKIE['theme']) ? ($_COOKIE['theme']==1?'black':'white') : 'black');
 
+/*	display date format	*/
 define("DATE_FORMAT","{{date}}/{{month}}/{{year}}");
-define("TIME_ZONE",'Australia/Melbourne');
 
-define("IMAGE_PATH",'images/dreams/');
+/*	timezone for dates	*/
+define("TIME_ZONE","Australia/Melbourne");
 
-define("ALCHEMY_API_KEY",'b66fcdf41275b600e1f08eae25f6498e55fce606');
+define("IMAGE_PATH","images/dreams/");
+
+define("ALCHEMY_API_KEY","");
 
 define("DB_HOST","localhost");
 define('DB_NAME',"artefacts");
 define("DB_USER","");
 define("DB_PASS","");
 
-/*	submitted dreams will be posted to TUMBLR_POST_EMAIL if true	*/
-define("POST_TO_TUMBLR",true);
-define("TUMBLR_POST_EMAIL","dgsnreawd6icq@tumblr.com");
+/*	submitted dreams will be posted to address specified in TUMBLR_POST_EMAIL if POST_TO_TUMBLR = true	*/
+define("POST_TO_TUMBLR",false);
+define("TUMBLR_POST_EMAIL","");
 
 define("MIN_TAG_VALUE",2);
 
 define("DEBUG",true);
 
-ini_set('error_reporting', 1);
+ini_set('error_reporting', 0);
 ?>
