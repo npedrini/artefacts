c3a3e0ea22d1b74e4dfa5f076001ccf7b8a530b1
David Patman <dpatman69@gmail.com>
2018-07-16 13:32:16 +1000

    Skip date range search if origin is set

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once "config/config.php";
 include_once "includes/session.php";
 
@@ -6,7 +6,7 @@ $date = new DateTime( 'now', new DateTimeZone(TIME_ZONE) );
 
 $showIntro = isset($_SESSION['introShown']) ? false : true;
 $expandRange = isset($_GET['expand']) ? $_GET['expand']==="true" : true;
-
+$dateSearch = isset($_GET['dateSearch']) ? $_GET['dateSearch']==="true" : true;
 $_SESSION['introShown'] = true;
 
 $stats = array();
@@ -16,25 +16,25 @@ if( $showIntro )
 	//	get stats
 	$mysqli = new mysqli( DB_HOST, DB_USER, DB_PASS );
 	$mysqli->select_db( DB_NAME );
-	
+
 	$sql = "SELECT COUNT(DISTINCT(dreams.id)) AS dream_count, ";
 	$sql .= "COUNT(DISTINCT(dreams.user_id)) AS dreamer_count, ";
 	$sql .= "COUNT(DISTINCT(dreams.occur_date)) AS dream_dates ";
 	$sql .= "FROM `dreams`";
-	
+
 	$result = $mysqli->query($sql);
 	$row = $result->fetch_assoc();
-	
+
 	$stats_temp = array
 	(
 		'dreams'=>$row['dream_count'],
 		'dreamers'=>$row['dreamer_count'],
 		'nights'=>$row['dream_dates']
 	);
-	
+
 	arsort($stats_temp);
-	
-	foreach($stats_temp as $label=>$value) 
+
+	foreach($stats_temp as $label=>$value)
 		$stats[] = $value . ' ' . $label;
 }
 ?>
@@ -53,9 +53,9 @@ if( $showIntro )
 </head>
 
 <body id="vis" style="margin: 0px;">
-	
+
 	<div id="intro" class="centered">
-	
+
 		<div>
 			<?php if( isset($_SESSION['origin']) && $_SESSION['origin']=='mona' ) { ?>
 			<span id="line1" class="emphasized"><a>Artefacts of the Collective Unconscious</a></span><br/><span id="line2">A repository of MONA visitor dreams, sponsored by MONA Market.</span><div style="margin-top:50px"><img src='images/mona.png'/></div>
@@ -65,83 +65,86 @@ if( $showIntro )
 			</a>
 			<?php } ?>
 		</div>
-		
+
 	</div>
-	
+
 	<div id="background"></div>
-	
+
 	<div id="foreground" style="height:100%">
-		
-		<?php 
+
+		<?php
 		if( !EMBEDDED )
-			include "includes/header.php"; 
+			include "includes/header.php";
 		?>
-		
+
 		<!-- d3 -->
 		<div id="visualization"></div>
-		
+
 		<div id="footer">
-			
+
 			<!-- controls -->
 			<div id="settings">
-				
+
 				<!-- search -->
+				<?php if($dateSearch) { ?>
 				<div id="search" class="setting" title="Search">
+					<?php} ?>
 					<div id="searchIcon"></div>
 				</div>
-				
+
 				<!-- theme toggle -->
 				<div id="theme" class="setting" title="Lights">
 					<div id="themeIcon"></div>
 				</div>
-				
+
 				<div id="save" class="setting" title="Export">
-					
+
 					<form id="formSave" action="svg.php" method="POST" target="_blank" style="margin:0px;">
 						<input type="hidden" id="data" name="data"></input>
 						<div id="saveIcon"></div>
 					</form>
-					
+
 				</div>
-				
+
 				<div id="help" class="setting" title="Help">
 					<div id="helpIcon"></div>
 				</div>
-				
+
 			</div>
 			<!-- end settings -->
-			
-			<div id="legend"></div>	
-			
+
+			<div id="legend"></div>
+
 		</div>
 		<!-- end footer -->
-		
+
 	</div>
 	<!-- end foreground -->
-	
+
+<?php if($dateSearch) { ?>
 	<div id="searchPane">
-		
+		<?php } ?>
 		<div class="header">
 			<a id="searchClose" href="#">Close</a>
 		</div>
-		
+
 		<div class="content">
 			<span>From:</span><input id="dateFrom" type="text" name="from" placeholder="From" style="display:inline-block" />
 			<span>To:</span><input id="dateTo" type="text" name="to" placeholder="To" style="display:inline-block" />
 			<input id="searchButton" type="button" value="Go" />
 		</div>
-		
+
 	</div>
-	
+
 	<canvas id="canvas"></canvas>
-	
+
 	<script type="text/javascript" src="js/lib/jquery-1.9.1.js"></script>
 	<script type="text/javascript" src="js/lib/jquery-ui-1.10.1.custom.min.js"></script>
 	<script type="text/javascript" src="js/lib/d3/d3.min.js"></script>
 	<script type="text/javascript" src="js/lib/jquery.tipsy.js"></script>
 	<script type="text/javascript" src="js/lib/jquery.cookie.js"></script>
-	<script type="text/javascript" src="js/lib/canvg/rgbcolor.js"></script> 
-	<script type="text/javascript" src="js/lib/canvg/canvg.js"></script> 
+	<script type="text/javascript" src="js/lib/canvg/rgbcolor.js"></script>
+	<script type="text/javascript" src="js/lib/canvg/canvg.js"></script>
 	<script type="text/javascript" src="js/graph.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
 	<script type="text/javascript">
@@ -152,6 +155,6 @@ if( $showIntro )
 	var defaultTheme = "<?php echo THEME; ?>";
 	var DATE_FORMAT = "<?php echo DATE_FORMAT; ?>";
 	</script>
-	
+	var dateSearch = <?php echo $dateSearch?'true':'false' ?>;
 </body>
 </html>
