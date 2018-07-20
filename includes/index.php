2eccc014b1bf4a6a66fcceca50880cb7710b1692
David Patman <dpatman69@gmail.com>
2018-07-20 22:51:43 +1000

    Updated sample text

--- includes/index.php
+++ includes/index.php
@@ -8,7 +8,7 @@ require 'vendor/autoload.php';
 include 'vendor/cognitivebuild/watsonphpsdk/Source/Services/NaturalLanguageUnderstanding.php';
 
 $nlu = new NaturalLanguageUnderstanding( WatsonCredential::initWithCredentials('apikey','YNeZEVJ0S8wzUUVANNn9z9qc74O13xux5ZF-mpeHNkyc') );
-$model = new AnalyzeModel('Watson PHP SDK for IBM Watson Developer Cloud.', [ 'keywords' => [ 'limit' => 5 ] ]);
+$model = new AnalyzeModel('Test this dream text for keywords', [ 'keywords' => [ 'limit' => 5 ] ]);
 $result = $nlu->analyze($model);
 echo $result->getContent();
 
