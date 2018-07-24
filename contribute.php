48baee5d84de046f7bdd1e6713e6a14b4e2545da
David Patman <dpatman69@gmail.com>
2018-07-24 18:22:45 +1000

    remove hidden 'origin' field from form

--- contribute.php
+++ contribute.php
@@ -229,7 +229,7 @@ var tagTimer;
 			<form method="post" enctype="multipart/form-data" accept-charset="UTF-8">
 
 				<input type="hidden" name="id" value="<?php echo isset($dream->id)?$dream->id:''; ?>"  />
-				<input type="hidden" name="origin" value="womenindesign2018"  />
+				
 				<div class="module" id="fields">
 
 					<div class='title'>Dream Information</div>
